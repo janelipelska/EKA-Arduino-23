c03a730caa240692f69cee3035bad5600bfef6b1
janelipelska <janeli.pelska@artun.ee>
2023-12-16 12:09:50 +0200

    cleaned the code :)

--- EKA-Arduino-23-1/EKA_Arduino_23_2/EKA_Arduino_23_2.ino
+++ EKA-Arduino-23-1/EKA_Arduino_23_2/EKA_Arduino_23_2.ino
@@ -1,17 +1,17 @@
-int myPhotoresistor = A0;
-bool stateOfServo = false;
-
 #include <Servo.h> // include Servo library
 
 Servo myservo;  // create object to control a servo
+
+int myPhotoresistor = A0;
+bool stateOfServo = false;
 
 int pos = 0;    // variable to store the servo position
 
 
 void setup() {
 
-Serial.begin(9600);
-myservo.attach(9); 
+  Serial.begin(9600);
+  myservo.attach(9);
 
 }
 
@@ -19,31 +19,23 @@ void loop() {
 
   int lightValue = analogRead(myPhotoresistor); // assign sensor value to variable
 
-if(lightValue<800) {
-
-  if(stateOfServo==false){
-
-for (pos = 0; pos <= 180; pos += 1) { // goes from 0-180 degrees
-myservo.write(pos); 
-delay(15);  
-Serial.println(lightValue);
-
-
-  }
-  stateOfServo = true;
-  }
-
-   } else{
-
-  if(stateOfServo==true){
-
-for (pos = 180; pos >= 0; pos -= 1) { 
-    myservo.write(pos);              
-    delay(15); // waits }
-  Serial.println(lightValue);
-
- }
-  stateOfServo = false;
-  }
-  }
-  }
+  if (lightValue<800) {
+    if (stateOfServo == false) { 
+      for (pos = 0; pos <= 180; pos += 1) { // goes from 0-180 degrees
+        myservo.write(pos); 
+        delay(15);  // waits (ms) 
+        Serial.println(lightValue);
+      }
+    stateOfServo = true;
+    }
+  } else {
+    if (stateOfServo == true) {
+      for (pos = 180; pos >= 0; pos -= 1) { 
+        myservo.write(pos);              
+        delay(15); // waits (ms) 
+        Serial.println(lightValue);
+      }
+    stateOfServo = false;
+    }
+  }  
+}
